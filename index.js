78190751b678deef0524018cbd275cb6e52a9809
Josh Bitossi <josh@QRA.LOCAL>
2017-01-17 10:32:17 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var authenticationSuccess = function() { console.log('Successful authentication'); };
+var authenticationFailure = function() { console.log('Failed authentication'); };
+
+function authorizeTrello() {
+  Trello.authorize({
+  type: 'popup',
+  name: 'Getting Started Application',
+  scope: {
+    read: 'true',
+    write: 'true' },
+  expiration: 'never',
+  success: authenticationSuccess,
+  error: authenticationFailure
+});
+}
